619c5aca6f9fd6aa37fa45d467bdd9b8fd945bb5
Andrew Byerly <adbyerly@comcast.net>
2017-08-17 10:53:10 -0500

    created directive
    (broken)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 angular.module("drinks", []);
 
-angular.module("drinks").controller(
-    "getIngredients", 
-    ['$scope', '$http', function($scope, $http){
-        $http.get("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").then(function(result){
-            $scope.ingredients = result.data;
-        }, function(error){
-            console.log(error.message);
-        });
-    }]
-);
+angular.module("drinks").directive("getIngredients", function(){
+    return {
+        templateUrl: "templates/ingredient-list.html",
+        controller: ['$scope', '$http', function($scope, $http){
+            
+            $scope.drinkList = [];
+            
+            $http.get("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").then(function(result){
+                $scope.ingredients = result.data;
+            }, function(error){
+                console.log(error.message);
+            });
+            
+            $scope.submitChoices = function() {
+                $scope.drinkForm;
+            };
+        }]
+    }
+});
